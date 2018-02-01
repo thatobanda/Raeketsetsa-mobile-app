4b6a74b83c2bfaf4df2dbd2c5be2f744682ddccc
thatobanda <33418732+thatobanda@users.noreply.github.com>
2018-02-01 09:07:55 +0200

    Create About.java

---
+++ app/src/main/java/de/andreasschrade/androidtemplate/ui/quote/About.java
@@ -0,0 +1,15 @@
+package de.andreasschrade.androidtemplate.ui.quote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.andreasschrade.androidtemplate.R;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
